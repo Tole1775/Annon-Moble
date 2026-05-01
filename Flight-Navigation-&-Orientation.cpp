f840be8468bfeb4a430526a787884ffbf38f0e71
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-05-01 01:51:08 -0700

    Create Flight-Navigation-&-Orientation.cpp from Flight-Navigation-&-Orientation.java

---
+++ Flight-Navigation-&-Orientation.cpp
@@ -0,0 +1,10 @@
+// Contents that were in Flight-Navigation-&-Orientation.java
+
+// Sample Content
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Flight Navigation and Orientation" << endl;
+    return 0;
+}
